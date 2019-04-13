843749da1fc9c33fdaf9f1d174c35e584987ba7a
aijunhao <1172949809@qq.com>
2019-04-13 20:15:02 +0800

    创建 NewsListApi 数据模拟接口

--- router.js
+++ router.js
@@ -3,10 +3,24 @@ var fs = require('fs')
 var path = require('path')
 var router = express.Router()
 
-// 首页
+// 轮播图
 router.get('/swipeItems', (req, res) => {
   fs.readFile(
     path.join(__dirname, '/data/swipeItemsApi.json'),
+    'utf-8',
+    (err, data) => {
+      if (err) {
+        return console.log('读取错误')
+      }
+      res.send(data)
+    }
+  )
+})
+
+// 新闻加载页面
+router.get('/newsList', (req, res) => {
+  fs.readFile(
+    path.join(__dirname, '/data/newsListApi.json'),
     'utf-8',
     (err, data) => {
       if (err) {
